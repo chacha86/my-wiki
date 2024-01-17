3bcc06e4687dcfb97c870d3dc7ba6d3535d8757c
chacha86 <taejincha0619@gmail.com>
2024-01-17 17:55:53 +0900

    ??

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,7 @@
 import {} from "./event_handler/event_search_box.js";
 import {} from "./event_handler/event_item_menu.js";
 import {} from "./event_handler/event_side_drag.js";
+import {} from "./event_handler/event_nav_toggle.js";
 import {NoteRenderer} from "./note/note_renderer.js";
 import {CommonRenderer} from "./common/common_renderer.js";
 
